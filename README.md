64e8c74280b91d1f353b6c655bae4fe8fd092561
Manda Li <leexiux@gmail.com>
2018-10-23 20:38:54 -0400

    save job metadata to csv

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # data-science-jobs
+This project is to analyze the most-wanted skills for data scientists and potentially which skills are linked with higher salaries. 
+
+TODOs:
+  - Scrape job meta data from indeed
+  - Scrape job details for each job
+  - Extract skills from job details
+  - Scrape job salaries (these may not be available from indeed so may need to scrape data from other sites such as glassdoor
+  - Analyze skills VS salaries
+  - More analysis to be defined :)
--- main.py
+++ main.py
@@ -7,20 +7,26 @@ import csv
 # if sys.setdefaultencoding() is not utf-8, set the encoding here
 #sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
-def getPageContent(url, useSaved = False):
-    if not useSaved:
-        response = requests.get(url)
-        if(response.ok):
-            content = response.content.decode('utf-8','ignore')
-        else:
-            print(response.content)
-        return response.ok, content
-    else:
-        file = open("page.html", 'r', encoding='utf-8')
-        content = file.read()
-        return True, content
+def getPageContent(url):
+    ''' Get HTML source for url
+    
+    :param url: url to parse 
+    :return: response object
+    :rtype: requests.Response
+    '''
+    response = requests.get(url)
+    if(response.ok):
+        response.decodedContent = response.content.decode('utf-8','ignore')
+    return response
 
 def getIndeedJobList(pageContent, exclude):
+    ''' Get Indeed Job meta data, including id, title, company, and location
+    
+    :param pageContent: HTML source of the indeed job list page
+    :param exclude: a list of job IDs to exclude (indeed puts sponsored jobs on the top of many different pages)
+    :return: a list of job meta data
+    :rtype: List
+    '''
     jobList = []
     for div in pageContent.find_all("div", class_='result'):
         if(div['id'] in exclude):
@@ -50,17 +56,27 @@ def getIndeedJobList(pageContent, exclude):
     
     return jobList
 
-def getJobs():
+def getJobs(numPages):
+    ''' driver function to get the indeed data scientist jobs
+    
+    :param numPages: scrape up to numPages 
+    '''
     baseURL = 'https://www.indeed.ca/data-scientist-jobs'
     url = baseURL
     start = 0
     jobList = []
     exclude = set()
     while(True):
+        # only process numPages
+        if(numPages is not None and start >= numPages):
+            break
         url = baseURL if start == 0 else baseURL + '?start='+str(start)
         print('Crawling ' + url)
-        ok, content = getPageContent(url)
-        pageContent = BeautifulSoup(content, 'html.parser', exclude_encodings=['gbk'])
+        response = getPageContent(url)
+        if(not response.ok):
+            print('Problem get page content from %s, code: %d, reason: %s' % (url, response.status_code, response.reason))
+            break
+        pageContent = BeautifulSoup(response.decodedContent, 'html.parser', exclude_encodings=['gbk'])
         jobs = getIndeedJobList(pageContent, exclude)
         jobList.extend(jobs)
         pagination_tag = pageContent.find_all(class_='pn')
@@ -75,7 +91,7 @@ def getJobs():
     if(len(jobList)>0):
         csvHeaders = jobList[0].keys()
         filePath = 'jobs.csv'
-        with open(filePath, 'w') as output_file:
+        with open(filePath, 'w', encoding='utf-8') as output_file:
             dict_writer = csv.DictWriter(output_file, csvHeaders)
             dict_writer.writeheader()
             dict_writer.writerows(jobList)
